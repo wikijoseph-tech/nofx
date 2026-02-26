37331a95473e1eaf321b3c6975c790e234dacc0a
fuyu.tan <wikijoseph8@gmail.com>
2026-02-26 22:09:30 +0900

    u

--- web/src/components/landing/FooterSection.tsx
+++ web/src/components/landing/FooterSection.tsx
@@ -9,16 +9,6 @@ interface FooterSectionProps {
 export default function FooterSection({ language }: FooterSectionProps) {
   const links = {
     social: [
-      { name: 'GitHub', href: OFFICIAL_LINKS.github, icon: Github },
-      {
-        name: 'X (Twitter)',
-        href: OFFICIAL_LINKS.twitter,
-        icon: () => (
-          <svg viewBox="0 0 24 24" className="w-4 h-4" fill="currentColor">
-            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-          </svg>
-        ),
-      },
       { name: 'Telegram', href: OFFICIAL_LINKS.telegram, icon: Send },
     ],
     resources: [
